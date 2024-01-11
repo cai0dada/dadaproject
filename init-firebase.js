bb62d16f857e72d9a82fe5ab1029d3fcc945d92d
cai0dada <125868317+cai0dada@users.noreply.github.com>
2024-01-11 23:09:50 +0800

    Add files via upload

---
+++ init-firebase.js
@@ -0,0 +1,12 @@
+// TODO: Replace the following with your app's Firebase project configuration
+
+var firebaseConfig = {
+    apiKey: "https://dadaserver-96b2b-default-rtdb.firebaseio.com/",
+    authDomain: "dadaserver-96b2b.firebaseapp.com",
+    databaseURL: "https://<project id>-default-rtdb.firebaseio.com/",
+    projectId: "dadaserver-96b2b",
+    storageBucket: "gs://<project id>.appspot.com/",
+};
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
